636d79bf28d114b9d4a5c5cfe87feb9f30d3fb3d
joelortiiz <daw_joelortiz@riberadeltajo.es>
2024-01-15 18:11:45 +0100

    Models

---
+++ lib/class/Hotel.php
@@ -0,0 +1,93 @@
+<?php
+
+
+class Hotel {
+    private $id;
+    private $nombre;
+    private $direccion;
+    private $ciudad;
+    private $pais;
+    private $num_habiaciones;
+    private $descripcion;
+    private $foto;
+    
+    public function __construct($id, $nombre, $direccion, $ciudad, $pais, $num_habiaciones, $descripcion, $foto) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->direccion = $direccion;
+        $this->ciudad = $ciudad;
+        $this->pais = $pais;
+        $this->num_habiaciones = $num_habiaciones;
+        $this->descripcion = $descripcion;
+        $this->foto = $foto;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getDireccion() {
+        return $this->direccion;
+    }
+
+    public function getCiudad() {
+        return $this->ciudad;
+    }
+
+    public function getPais() {
+        return $this->pais;
+    }
+
+    public function getNum_habiaciones() {
+        return $this->num_habiaciones;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function getFoto() {
+        return $this->foto;
+    }
+
+    public function setId($id): void {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setDireccion($direccion): void {
+        $this->direccion = $direccion;
+    }
+
+    public function setCiudad($ciudad): void {
+        $this->ciudad = $ciudad;
+    }
+
+    public function setPais($pais): void {
+        $this->pais = $pais;
+    }
+
+    public function setNum_habiaciones($num_habiaciones): void {
+        $this->num_habiaciones = $num_habiaciones;
+    }
+
+    public function setDescripcion($descripcion): void {
+        $this->descripcion = $descripcion;
+    }
+
+    public function setFoto($foto): void {
+        $this->foto = $foto;
+    }
+
+    public function __destruct() {
+        
+    }
+
+}
